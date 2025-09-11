54bd837472757d4bce44179261cd7dd24be6efa2
Milesenberg <milesenbergers@gmail.com>
2025-09-11 19:57:59 +0100

    Updated files via GUI

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ rss_feeds = [
     "https://www.aljazeera.com/where/palestine/rss.xml",
     "https://blog.playstation.com/feed",
     "https://aiartblog.com/feed",
-    "http://bristolpost.co.uk/news/?service=rss",
     "http://bristolpost.co.uk/whats-on/?service=rss",
     "https://www.somersetlive.co.uk/?service=rss",
     "https://netpol.org/feed/",
